e62579eca841c93dfde94ee026bc1bc1d46e6d93
kaushalshakya <kshakya101@gmail.com>
2024-05-16 13:50:59 +0545

    docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
-FROM ubuntu
+FROM node:latest
 
-RUN apt-get update
-RUN apt-get install -y curl
-RUN curl -sL https://deb.nodesource.com/setup_20.x -o /tmp/nodesource_setup.sh
-RUN apt-get update
-RUN apt-get upgrade -y
-RUN apt-get install -y nodejs
+WORKDIR /usr/src/app
 
-COPY package.json package.json
+COPY package*.json /
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 4000
+
+CMD ["npm","run","dev"]
---
+++ models/user.model.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  profile_pic: {
+    type: String,
+  },
+});
+
+export const User = mongoose.model("user", userSchema);
